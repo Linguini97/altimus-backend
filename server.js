f811edf640647a245fc677cf4bb49a3b30d2e7c0
Linguini97 <vfo979797@gmail.com>
2025-02-12 01:06:03 -0300

    Troca de porta do servidor

--- server.js
+++ server.js
@@ -5,8 +5,7 @@ const bodyParser = require("body-parser");
 require("dotenv").config();
 
 const app = express();
-const PORT = process.env.PORT || 3000;
-
+const PORT = process.env.PORT || 8080;
 // 📌 Middleware
 app.use(cors({ origin: "*", methods: ["GET", "POST"], allowedHeaders: ["Content-Type"] }));
 app.use(bodyParser.json());
